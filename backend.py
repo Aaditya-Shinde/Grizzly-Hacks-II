cf021de16ce83190762846f8bb82db993d48e918
Aaditya Shinde <brightsun004@gmail.com>
2026-02-21 12:48:39 -0800

    WORKING IMAGE TRANFER TO PYTHON

--- backend.py
+++ backend.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify, render_template
+import cv2
+import base64
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,11 +14,30 @@ def index():
 def history():
     return render_template('history.html')
 
+import cv2
+import numpy as np
+import base64
+
+def data_url_to_cv2(data_url):
+    header, encoded = data_url.split(",", 1)
+    data = base64.b64decode(encoded)
+    
+    # Convert bytes to a numpy array
+    nparr = np.frombuffer(data, np.uint8)
+    
+    # Decode the image array into a CV2 BGR image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
 @app.route('/get_text', methods=['POST'])
 def get_text():
     data = request.get_json()
     
-    user_string = data.get('image')
+    imageData = data.get('image')
+
+    frame = data_url_to_cv2(imageData)
+    cv2.imwrite("debug_frame.jpg", frame)
+    cv2.waitKey(0)
 
     print(f"Python received: '{user_string}'")
     
