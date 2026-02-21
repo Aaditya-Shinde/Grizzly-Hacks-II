52a7ec0f2910248c2a7073132efd1ca55afe835a
Aaditya Shinde <brightsun004@gmail.com>
2026-02-21 14:38:21 -0800

    Direct js recognition

--- static/script.js
+++ static/script.js
@@ -1,54 +1,75 @@
-const video = document.getElementById('webcam');
-const canvas = document.getElementById('cam-display');
-const ctx = canvas.getContext('2d');
-const dpr = window.devicePixelRatio || 1;
-const rect = canvas.getBoundingClientRect();
+import {
+    GestureRecognizer,
+    FilesetResolver,
+    DrawingUtils
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
 
-canvas.width = rect.width * dpr;
-canvas.height = rect.height * dpr;
-ctx.scale(dpr, dpr);
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("cam-display");
+const canvasCtx = canvasElement.getContext("2d");
 
-document.getElementById('start-btn').addEventListener('click', () => {startCamera();});
+let gestureRecognizer;
+let lastVideoTime = -1;
 
-function startCamera() {
-    navigator.mediaDevices.getUserMedia({video:true})
-        .then(stream => {
-            video.srcObject = stream;
-            video.addEventListener('loadedmetadata', () => {
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
-            });
 
-            console.log("Camera successfully started.");
-            document.getElementById('start-btn').innerText = "Camera Active";
-            document.getElementById('start-btn').style.backgroundColor = "#28a745"
-        })
-        .catch(err => console.error("Camera error: ", err));
+//#region init
+function startWebcam(){
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predictWebcam);
+    });
 }
 
-async function getData(){
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+async function init(){
+    const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+    );
     
-    var imageData = canvas.toDataURL('image/jpeg', 1);
-    return imageData;
-}
-
-async function sendToPython() {
-    var imageData = await getData();
-    console.log("Value being sent:", imageData);
-
-    const response = await fetch('/get_text', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ "image": imageData })
+    gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+            delegate: "GPU"
+        },
+        runningMode: "LIVE_STREAM",
+        numHands: 2
     });
 
-    if (response.ok) {
-        const data = await response.json();
-        document.getElementById('pyRes').innerText = data.message;
-    } else {
-        console.error("Server returned an error:", response.status);
+    startWebcam();
+}
+//#endregion
+
+async function predictWebcam() {
+    // Sync canvas size to video size
+    canvasElement.width = video.videoWidth;
+    canvasElement.height = video.videoHeight;
+
+    let nowInMs = Date.now();
+    if (lastVideoTime !== video.currentTime) {
+        lastVideoTime = video.currentTime;
+        const results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+        const drawingUtils = new DrawingUtils(canvasCtx);
+
+        if (results.gestures.length > 0) {
+            const categoryName = results.gestures[0][0].categoryName;
+            const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2);
+            const handedness = results.handednesses[0][0].displayName;
+
+            console.log(`Gesture: ${categoryName} (${handedness}) \n Confidence: ${categoryScore}%`);
+
+            for (const landmarks of results.landmarks) {
+                drawingUtils.drawConnectors(landmarks, GestureRecognizer.HAND_CONNECTIONS, {
+                    color: "#00FF00",
+                    lineWidth: 5
+                });
+                drawingUtils.drawLandmarks(landmarks, { color: "#FF0000", lineWidth: 2 });
+            }
+        } else {
+            console.log("No hand detected");
+        }
     }
+    window.requestAnimationFrame(predictWebcam);
 }
 
-// setInterval(sendToPython, 100);
+init();
